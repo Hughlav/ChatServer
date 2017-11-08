7184114a2d0206837df7b6ab1578001f02618301
HughLav <HughLavery@Hughs-MacBook-Pro.local>
2017-11-08 13:57:51 +0000

    added room data type and client data type. added methods to creating client and sending message to all members of a room.

--- src/Main.hs
+++ src/Main.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE RecordWildCards #-} 
 module Main where
   
   import Network.Socket
@@ -6,27 +7,34 @@ module Main where
   import Control.Concurrent
   import Control.Monad (when)
   import Control.Monad.Fix (fix)
+  import Control.Applicative
+  import Control.Concurrent.STM
+  import Data.Map (Map)
+  import qualified Data.Map as M
+  import Data.Set (Set)
+  import qualified Data.Set as S
+
   
   {-
   To join a chat client sends
         JOIN_CHATROOM: [chatroom name]
         CLIENT_IP: [IP Address of client if UDP | 0 if TCP]
         PORT: [port number of client if UDP | 0 if TCP]
-            CLIENT_NAME: [string Handle to identifier client user]
+        CLIENT_NAME: [string Handle to identifier client user]
   
   Server responds with
         JOINED_CHATROOM: [chatroom name]
         SERVER_IP: [IP address of chat room]
         PORT: [port number of chat room]
         ROOM_REF: [integer that uniquely identifies chat room on server]
-            JOIN_ID: [integer that uniquely identifies client joining]
+        JOIN_ID: [integer that uniquely identifies client joining]
             
   Server should also let chat room know that "CLIENT_NAME" joint chat room
   
   To leave a chatroom client sends 
         LEAVE_CHATROOM: [ROOM_REF]
         JOIN_ID: [integer previously provided by server on join]
-            CLIENT_NAME: [string Handle to identifier client user]
+        CLIENT_NAME: [string Handle to identifier client user]
             
   The server responds with the following message:
         LEFT_CHATROOM: [ROOM_REF]
@@ -37,13 +45,13 @@ module Main where
   To terminate connection Client sends:
         DISCONNECT: [IP address of client if UDP | 0 if TCP]
         PORT: [port number of client it UDP | 0 id TCP]
-            CLIENT_NAME: [string handle to identify client user]
+        CLIENT_NAME: [string handle to identify client user]
             
   To send a message Client sends:
-            CHAT: [ROOM_REF]
+        CHAT: [ROOM_REF]
         JOIN_ID: [integer identifying client to server]
         CLIENT_NAME: [string identifying client user]
-            MESSAGE: [string terminated with '\n\n']
+        MESSAGE: [string terminated with '\n\n']
             
   Server sents chat group:
         CHAT: [ROOM_REF]
@@ -56,10 +64,37 @@ module Main where
   
   -}
   
+  type ClientName = String
+  type RoomName = String
+
+
+--check
+  type ErrorHeading = String
+  type ErrorBody = String
+  type CmdArgs = [[String]]
+
+  data Message = Notice String
+      | Tell ClientName String
+      | Broadcast ClientName String
+      | Command CmdArgs String
+      | Error ErrorHeading ErrorBody
+
+
+
+  data Client = Client
+      { clientName :: ClientName
+      , clientChan :: TChan Message
+      , clientHandle :: Handle 
+  }
   
-  
+  data Room = Room
+      { roomName :: RoomName
+      , roomID :: Int
+      , clients :: TVar (Map Int Client) -- Store list of clients in room
+  }
   
   type Msg = (Int, String)
+  type RoomList = TVar (Map Int Room) -- Store list of rooms
   
   main :: IO()
   main = do
@@ -115,7 +150,26 @@ module Main where
       broadcast ("<-- " ++ name ++ " left.") -- make a final broadcast
       hClose hdl                             -- close the handle
   
-  
-  
-  
+
+--do i need ID
+  createClient :: ClientName -> Handle -> IO Client
+  createClient name handle = do
+      chan <- newTChanIO
+      return Client { clientName = name
+                    , clientChan = chan
+                    , clientHandle = handle
+                    }
+
+  sendMsg :: Client -> Message -> STM ()
+  sendMsg Client{..} msg = writeTChan clientChan msg -- {..} pattern matching so clientChan is easily accesible 
       
+  sendMsgtoRoom :: Message -> Room -> IO ()
+  sendMsgtoRoom msg room@Room{..} = atomically $ do 
+      clientList <- readTVar clients --return a list of clients in a room from clients TVar
+      let roomClients = M.elems clientList --access properties of clients
+      mapM_ (\a -> sendMsg a msg) roomClients --send the message to each member in the room
+  
+  
+  
+  
+  --isInfixOf "Join" msgfromclient --returns true if join is in msg. use this to handle messages. where though?
